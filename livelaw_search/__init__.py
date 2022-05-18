462045e3eb9bd677bf56d05f67204b32265a28a1
Rose <rosemary.joseph@simelabs.com>
2022-05-18 15:13:18 +0530

    modified files

--- livelaw_search/__init__.py
+++ livelaw_search/__init__.py
@@ -1,19 +1,18 @@
-import os
-from dotenv import load_dotenv
-from flask import Flask
-from elasticsearch import Elasticsearch
+from flask import Flask, redirect, url_for
 
-load_dotenv('.env')
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    
     from . import search
     app.register_blueprint(search.bp)
-    
-    # a simple page that says hello
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
 
+    # a simple page that says hello
+    @app.route("/")
+    def hello():
+        return redirect(url_for("livelaw.search"))
+        
     return app
+
+
+   
--- livelaw_search/search.py
+++ livelaw_search/search.py
@@ -1,19 +1,17 @@
 import os
-from re import search
-from urllib import response
 from flask import (
-    Flask,
     render_template,
     request,
     Blueprint,
-    redirect,
 )
 from elasticsearch import Elasticsearch
 
 ES_HOST = os.environ.get("ES_HOST")
-es = Elasticsearch("http://52.7.83.219:9200")
+es = Elasticsearch("http://localhost:9200")
 
 bp = Blueprint("livelaw", __name__, url_prefix="/news")
+
+
 @bp.route("/search/", methods=("GET", "POST"))
 def search():
     """Function to search and load data with pagination"""
@@ -42,6 +40,7 @@ def search():
         search_count = page["hits"]["total"]["value"]
 
         return render_template(
-            "search.html", search_result=search_result, search=search, page=int(current_page) + 1, search_count=search_count
+            "search.html", search_result=search_result, search=search,
+            page=int(current_page) + 1, search_count=search_count
         )
     return render_template("search.html")
